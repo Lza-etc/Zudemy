4474e9d3d49c3e749918693478cf562b5060d818
Lza-etc <emj688531@gmail.com>
2023-09-08 15:59:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Zudemy
+### E-Learning Mobile Responsive Website Model
+### Built for a web design competition
+
+## Installation/Usage:
+
+First and foremost, you have to install __[node.js](https://nodejs.org/en/)__ on your machine (if, it is not already installed), then follow these 4 simple steps below:
+
+**Step 1**: Clone this entire repo
+```bash
+$ git clone 
+```
+
+**Step 2**: Edit the directory name or Move into the directory as it is
+```bash
+$ cd Zudemy
+```
+
+**Step 3**: Install dependencies
+```bash
+$ npm install
+```
+
+**Step 4**: Run the example
+```bash
+$ npm start
+```
+
+### Additional Info:
+
+
+
+Your browser will open....Now edit `index.html` and/or `styles.css` and/or `scripts.js` file inside `Zudemy` directory...:heart: :heart: :heart:
---
+++ scripts.js
@@ -0,0 +1,95 @@
+$('#popoverData').popover();
+$('#popoverOption').popover({ trigger: "hover" });
+
+$('#react').popover();
+$('#popoverOption').popover({ trigger: "hover" });
+
+$('#ai').popover();
+$('#popoverOption').popover({ trigger: "hover" });
+
+$('#unreal').popover();
+$('#popoverOption').popover({ trigger: "hover" });
+
+$('#android').popover();
+$('#popoverOption').popover({ trigger: "hover" });
+
+$('#join').popover();
+$('#popoverOption').popover({ trigger: "hover" });
+
+$('#adobe').popover();
+$('#popoverOption').popover({ trigger: "hover" });
+
+
+$('#sketch').popover();
+$('#popoverOption').popover({ trigger: "hover" });
+
+
+$('#gra').popover();
+$('#popoverOption').popover({ trigger: "hover" });
+
+
+$('#ill').popover();
+$('#popoverOption').popover({ trigger: "hover" });
+
+
+
+$('#digi').popover();
+$('#popoverOption').popover({ trigger: "hover" });
+
+
+$('#mark').popover();
+$('#popoverOption').popover({ trigger: "hover" });
+
+
+$('#face').popover();
+$('#popoverOption').popover({ trigger: "hover" });
+
+$('#insta').popover();
+$('#popoverOption').popover({ trigger: "hover" });
+
+$('#socio').popover();
+$('#popoverOption').popover({ trigger: "hover" });
+
+$('#baa').popover();
+$('#popoverOption').popover({ trigger: "hover" });
+
+
+
+$('#bbb').popover();
+$('#popoverOption').popover({ trigger: "hover" });
+
+
+
+$('#bcc').popover();
+$('#popoverOption').popover({ trigger: "hover" });
+
+
+
+$('#bdd').popover();
+$('#popoverOption').popover({ trigger: "hover" });
+
+
+
+$('#bee').popover();
+$('#popoverOption').popover({ trigger: "hover" });
+
+$('#fgg').popover();
+$('#popoverOption').popover({ trigger: "hover" });
+
+$('#fhh').popover();
+$('#popoverOption').popover({ trigger: "hover" });
+
+
+$('#fii').popover();
+$('#popoverOption').popover({ trigger: "hover" });
+
+
+$('#fjj').popover();
+$('#popoverOption').popover({ trigger: "hover" });
+
+
+$('#fkk').popover();
+$('#popoverOption').popover({ trigger: "hover" });
+
+
+ 
